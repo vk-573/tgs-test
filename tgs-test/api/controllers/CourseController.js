b53208bad79872ee815238851d4e7b435c4c11f1
VK <valentin.kiryk@gmail.com>
2020-04-07 18:56:29 +0200

    add($Validator): adding validatorService for courses && users
    Adding signup, login and courses request body parameters validation
    implementing it in courseController

--- tgs-test/api/controllers/CourseController.js
+++ tgs-test/api/controllers/CourseController.js
@@ -1,11 +1,30 @@
 const CourseService = require('../services/CourseService');
+const { coursesValidation } = require('../services/ValidatorService');
 
 module.exports = {
+
+    // get courses with body params
     get: function(req, res) {
         console.log("get courses");
+
+        // filtering request data
         const data = {
-            test: req.body.test,
+            startLatitude: req.body.startLatitude,
+            startLongitude: req.body.startLongitude,
+            endLatitude: req.body.endLatitude,
+            endLongitude: req.body.endLongitude,
+            startFullAddress: req.body.startFullAddress,
+            startZipCode: req.body.startZipCode,
+            endFullAddress: req.body.endFullAddress,
+            startDate: req.body.startDate,
         }
+
+        // request data validation
+        const {error} = coursesValidation(data);
+        if (error) {
+            return res.status(400).send(error.message);
+        }
+
         CourseService.get(data).then((courses) => {
             console.log("in res controller:", courses);
             return res.status(200).send(courses);
@@ -16,4 +35,3 @@ module.exports = {
         });
     },
 };
-
---
+++ tgs-test/api/services/ValidatorService.js
@@ -0,0 +1,41 @@
+const Joi = require ("@hapi/joi");
+
+// User
+const signupValidation = data => {
+    const userCreation = Joi.object({
+        email:              Joi.string().required().min(4).max(128).email().lowercase(),
+        hash:               Joi.string().required().min(9).max(128),
+    });
+    return userCreation.validate(data);
+}
+const loginValidation = data => {
+    const userLogin = Joi.object({
+        email:              Joi.string().min(4).max(128).email().lowercase().required(),
+        password:           Joi.string().required().max(128),
+    });
+    return userLogin.validate(data);
+}
+
+// Courses
+const coursesValidation = data => {
+    const course = Joi.object({
+        startLatitude:      Joi.number().required(),
+        startLongitude:     Joi.number().required(),
+        endLatitude:        Joi.number().required(),
+        endLongitude:       Joi.number().required(),
+        startFullAddress:   Joi.string().allow('').optional().max(300),
+        startZipCode:       Joi.string().allow('').optional().max(32),
+        endFullAddress:     Joi.string().allow('').optional().max(300),
+        startZipCode:       Joi.string().allow('').optional().max(32),
+        startDate:          Joi.string().allow('').optional().max(56),
+    });
+    return course.validate(data);
+}
+
+module.exports = {
+    // User
+    signupValidation,
+    loginValidation,
+    // Courses
+    coursesValidation,
+};
