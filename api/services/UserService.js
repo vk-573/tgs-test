8cbabab3c27e4e181b8827930016ded62b469416
VK <valentin.kiryk@gmail.com>
2020-04-08 16:30:45 +0200

    docs(apiDocs): adding api routes documentation && postman import file
    added apiDocs.docx
    added TGS-test.postman_collection.json

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -5,9 +5,10 @@ module.exports = {
         try {
             let usedEmail = await User.findOne({ email: data.email });
             if (usedEmail) {
-                throw new TgsError(400, 'email is taken');
+                throw new TgsError(400, 'Email Taken');
             }
             let newUser = await User.create({email: data.email, password: data.password}).fetch();
+            newUser.password = undefined;
             return newUser;
         } catch (err) {
             throw (err);
@@ -18,10 +19,10 @@ module.exports = {
         try {
             let user = await User.findOne({ email: data.email });
             if (!user) {
-                throw new TgsError(400, 'email not found');
+                throw new TgsError(404, 'User Not Found');
             }
             if (user.password !== data.password) {
-                throw new TgsError(400, 'invalid password');
+                throw new TgsError(400, 'Invalid Password');
             }
             return user;
         } catch (err) {
